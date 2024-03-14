44b82ec15edf0690a4d7370836abaf8b8467cd93
Ginga Alexandra <alexandra.ginga@stud.ubbcluj.ro>
2024-03-14 02:01:19 +0200

    2:01 am

--- LicentaApp/PachetPage.xaml.cs
+++ LicentaApp/PachetPage.xaml.cs
@@ -15,59 +15,20 @@ public partial class PachetPage : ContentPage
         Pachet pachet = new Pachet();
         BindingContext = pachet;
         LoadData();
+        LoadCategorie();
+        
     }
-
-    async void OnSaveButtonClicked(object sender, EventArgs e)
-    {
-        try
-        {
-            var slist = (Pachet)BindingContext;
-
-            if (slist != null)
-            {
-                // Obiectul BindingContext nu este nul, poți continua cu operațiunile dorite.
-                await App.DatabasePachet.SavePachetAsync(slist);
-                await Navigation.PopAsync();
-            }
-            else
-            {
-                // Obiectul BindingContext este nul, afișează un mesaj de eroare sau iei alte măsuri.
-                Console.WriteLine("Obiectul BindingContext este nul în OnSaveButtonClicked.");
-            }
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"Eroare la salvare: {ex.Message}");
-        }
-    }
-
-
-    async void OnDeleteButtonClicked(object sender, EventArgs e)
-    {
-        try
-        {
-            var plist = (Pachet)BindingContext;
-            await App.DatabasePachet.DeletePachetAsync(plist);
-            await Navigation.PopAsync();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Eroare în OnSaveButtonClicked: {ex.Message}");
-        }
-
-    }
-    //ce mi a dat pe chat, necesita rearanjare
     private async void LoadData()
     {
         try
         {
             // Assuming GetLocationsAsync is a method in App.DatabaseRestaurant that retrieves locations
-            var locations = await App.DatabaseRestaurant.GetRestaurantAsync();
+            var locatie = await App.DatabaseRestaurant.GetRestaurantAsync();
 
-            if (locations != null && locations.Any())
+            if (locatie != null && locatie.Any())
             {
                 // Bind the retrieved locations to the Picker
-                picker.ItemsSource = locations;
+                picker.ItemsSource = locatie;
             }
             else
             {
@@ -80,6 +41,70 @@ public partial class PachetPage : ContentPage
             Console.WriteLine($"Error loading data: {ex.Message}");
         }
 
+    }
+
+    private async void LoadCategorie()
+    {
+        try
+        {
+            // Assuming GetLocationsAsync is a method in App.DatabaseRestaurant that retrieves locations
+            var eveniment = await App.Database.GetCategoryAsync();
+
+            if (eveniment != null && eveniment.Any())
+            {
+                // Bind the retrieved locations to the Picker
+                pickercategory.ItemsSource = eveniment;
+            }
+            else
+            {
+                // Handle the case where no locations are available
+                // You might want to show an error message or take appropriate action
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error loading data: {ex.Message}");
+        }
 
     }
-}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var slist = (Pachet)BindingContext;
+
+                if (slist != null)
+                {
+                    // Obiectul BindingContext nu este nul, poți continua cu operațiunile dorite.
+                    await App.DatabasePachet.SavePachetAsync(slist);
+                    await Navigation.PopAsync();
+                }
+                else
+                {
+                    // Obiectul BindingContext este nul, afișează un mesaj de eroare sau iei alte măsuri.
+                    Console.WriteLine("Obiectul BindingContext este nul în OnSaveButtonClicked.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Eroare la salvare: {ex.Message}");
+            }
+        }
+
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var plist = (Pachet)BindingContext;
+                await App.DatabasePachet.DeletePachetAsync(plist);
+                await Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Eroare în OnSaveButtonClicked: {ex.Message}");
+            }
+
+        }
+    }
+
